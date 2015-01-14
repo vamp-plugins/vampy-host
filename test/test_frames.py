3155a42df11b371d3b026dd928d0abc5c373e5c9
Chris Cannam <c.cannam@qmul.ac.uk>
2015-01-14 09:52:33 +0000

    Start Python module, add framer

---
+++ test/test_frames.py
@@ -0,0 +1,22 @@
+
+import vamp
+import numpy as np
+
+def to_lists(arrs):
+    return [list([list(r) for r in f]) for f in arrs]
+
+def test_frames_from_1d_buffer():
+    buf = np.arange(6)
+    ff = to_lists(vamp.framesFromArray(buf, 2, 2))
+    assert(ff == [[[0,1]],[[2,3]],[[4,5]]])
+    ff = to_lists(vamp.framesFromArray(buf, 1, 2))
+    assert(ff == [[[0,1]],[[1,2]],[[2,3]],[[3,4]],[[4,5]],[[5,0]]])
+
+def test_frames_from_2d_buffer():
+    buf = np.array([np.arange(6),np.arange(6,12)])
+    ff = to_lists(vamp.framesFromArray(buf, 2, 2))
+    assert(ff == [[[0,1],[6,7]],[[2,3],[8,9]],[[4,5],[10,11]]])
+    ff = to_lists(vamp.framesFromArray(buf, 1, 2))
+    assert(ff == [[[0,1],[6,7]],[[1,2],[7,8]],[[2,3],[8,9]],[[3,4],[9,10]],[[4,5],[10,11]],[[5,0],[11,0]]])
+
+    
