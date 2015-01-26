13c96d7638f863a9d164558d21d2b16d5f49a661
Chris Cannam <c.cannam@qmul.ac.uk>
2015-01-26 12:28:30 +0000

    Tests for timestamp filling

--- test/test_collect.py
+++ test/test_collect.py
@@ -14,6 +14,7 @@ rate = 44100
 # expected to follow the norm
 
 blocksize = 1024
+eps = 1e-6
 
 def input_data(n):
     # start at 1, not 0 so that all elts are non-zero
@@ -31,3 +32,32 @@ def test_collect_one_sample_per_step():
     for r in results:
         assert r["timestamp"] == vamp.vampyhost.frame_to_realtime(r["values"][0], rate)
 
+def test_collect_fixed_sample_rate():
+    buf = input_data(blocksize * 10)
+    results = list(vamp.collect(buf, rate, plugin_key, "curve-fsr"))
+    assert len(results) == 10
+    i = 0
+    for r in results:
+        assert r["timestamp"] == vamp.vampyhost.RealTime('seconds', i * 0.4)
+        assert abs(r["values"][0] - i * 0.1) < eps
+        i = i + 1
+
+def test_collect_fixed_sample_rate_2():
+    buf = input_data(blocksize * 10)
+    results = list(vamp.collect(buf, rate, plugin_key, "curve-fsr-timed"))
+    assert len(results) == 10
+    i = 0
+    for r in results:
+        assert r["timestamp"] == vamp.vampyhost.RealTime('seconds', i * 0.4)
+        assert abs(r["values"][0] - i * 0.1) < eps
+        i = i + 1
+        
+def test_collect_variable_sample_rate():
+    buf = input_data(blocksize * 10)
+    results = list(vamp.collect(buf, rate, plugin_key, "curve-vsr"))
+    assert len(results) == 10
+    i = 0
+    for r in results:
+        assert r["timestamp"] == vamp.vampyhost.RealTime('seconds', i * 0.75)
+        assert abs(r["values"][0] - i * 0.1) < eps
+        i = i + 1
